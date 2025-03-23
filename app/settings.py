f0b904c0f03c0dae271c5706e885e0c5fe1a5626
neto test <kantimirov2018@yandex.ru>
2025-03-23 14:34:27 +0500

    debug-toolbar added

--- app/settings.py
+++ app/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'debug_toolbar',
+
+
     'main',
     'goods',
 ]
@@ -51,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls'
@@ -125,6 +130,7 @@ STATICFILES_DIRS: list = [
     BASE_DIR/ 'static'
     ]
 
+INTERNAL_IPS = ['127.0.0.1']
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
